7d4724baccd82af406a26587ea1347e5316f8cc0
AfjalAura9 <afjalshaiktechy@gmail.com>
2025-05-25 21:26:51 +0530

    ProfileHeader Responsive

--- src/components/UserProfile/ProfileHeader.jsx
+++ src/components/UserProfile/ProfileHeader.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const ProfileHeader = ({ user }) => {
   return (
-    <div className="flex items-center space-x-6">
+    <div className="flex flex-col sm:flex-row items-center sm:space-x-6 space-y-4 sm:space-y-0">
       <img
         src="../../../public/Logo.png"
         alt="User Avatar"
-        className="w-24 h-24 rounded-full border-2 border-gray-300"
+        className="w-20 h-20 sm:w-24 sm:h-24 rounded-full border-2 border-gray-300"
       />
-      <div>
-        <h2 className="text-2xl font-bold text-gray-800">
+      <div className="text-center sm:text-left">
+        <h2 className="text-xl sm:text-2xl font-bold text-gray-800">
           {user.firstName} {user.lastName}
         </h2>
         <p className="text-gray-600">{user.role}</p>
