0de939aa5a94925b59cd4bc0c9e3ddd30fa19e91
AfjalAura9 <afjalshaiktechy@gmail.com>
2025-05-27 11:34:28 +0530

    App.jsx Fix-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Login from "./components/Auth/Login";
 import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
 import AdminDashboard from "./components/Dashboard/AdminDashboard";
